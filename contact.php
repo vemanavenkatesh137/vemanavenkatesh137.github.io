1ea6c067d399466b35c8a1808f353e5c95e9828e
vemanavenkatesh137 <71435468+vemanavenkatesh137@users.noreply.github.com>
2023-03-02 18:38:55 +0530

    Update contact.php
    Updated details in contact.php form

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'vemanavenkatesh137@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = '../vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
@@ -27,7 +27,7 @@
   
   $contact->smtp = array(
     'host' => 'smtp.gmail.com',
-    'username' => 'portifolio',
+    'username' => 'vemanavenkatesh137@gmail.com',
     'password' => 'pcgrkchknzuhhvot',
     'port' => '587'
   );
