a522232aebf33ce4bc89e9947fc68d94a9efbfdc
vemanavenkatesh137 <71435468+vemanavenkatesh137@users.noreply.github.com>
2023-03-03 18:28:53 +0530

    Update contact.php
    Updated contact.php

--- contact.php
+++ contact.php
@@ -7,35 +7,20 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'vemanavenkatesh137@gmail.com';
+  $to = 'vemanavenkatesh137@gmail.com';
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+$from = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$subject = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_SPECIAL_CHARS);
+$message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_SPECIAL_CHARS);
 
-  if( file_exists($php_email_form = '../vendor/php-email-form/validate.js' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+if (filter_var($from, FILTER_VALIDATE_EMAIL)) {
+    $headers = ['From' => ($name?"<$name> ":'').$from,
+            'X-Mailer' => 'PHP/' . phpversion()
+           ];
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  
-  $contact->smtp = array(
-    'host' => 'smtp.gmail.com',
-    'username' => 'vemanavenkatesh137@gmail.com',
-    'password' => 'pcgrkchknzuhhvot',
-    'port' => '587'
-  );
-  
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
-?>
+    mail($to, $subject, $message."\r\n\r\nfrom: ".$_SERVER['REMOTE_ADDR'], $headers);
+    die('OK');
+    
+} else {
+    die('Invalid address');
+}
